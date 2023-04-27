09a118f71a269c16dce5a5adf44aa35848697d41
Michel Maia <programador.michel.maia@gmail.com>
2023-04-27 17:01:49 -0300

    commit
    aula 01

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Memoteca Angular Alura
  Desenvolvido em aula
+
+
+
+html / css
+https://github.com/alura-cursos/2438-angular-memoteca/blob/aula-1/src/app/componentes/cabecalho/cabecalho.component.html
---
+++ memoteca/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { TituloComponent } from './componentes/titulo/titulo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    TituloComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
