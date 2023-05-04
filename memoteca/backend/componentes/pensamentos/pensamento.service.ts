a6e01b1679a533c5099ae2569a7f575a61d17038
Michel Maia <programador.michel.maia@gmail.com>
2023-05-04 17:42:41 -0300

    commit

---
+++ memoteca/backend/componentes/pensamentos/pensamento.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Pensamento } from 'src/app/componentes/pensamentos/pensamento';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PensamentoService {
+
+  private readonly API = 'http://localhost:3000/pensamento'
+  constructor(private http: HttpClient) { }
+
+  listar() {
+    return this.http.get<Pensamento[]>(this.API)
+  }
+}
+
+
--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from 'backend/componentes/pensamentos/pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,27 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passa informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Propriedade é decorada com @input',
-      autoria: 'componente filho',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, non aliquam laborum voluptatum eius assumenda, architecto incidunt nam placeat et, sequi magni facilis asperiores officiis doloribus. Autem quasi iure voluptate.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, non aliquam laborum voluptatum eius assumenda, architecto incidunt nam placeat et, sequi magni facilis asperiores officiis doloribus. Autem quasi iure voluptate.',
-      autoria: 'teste',
-      modelo: 'modelo1'
-    }
-   ];
+  listaPensamentos: Pensamento[] = [];
 
-  constructor() { }
+  constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
+    this.service.listar()
   }
 
 }
