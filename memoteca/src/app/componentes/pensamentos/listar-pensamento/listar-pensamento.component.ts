c6fd697fc080155dc3bcb1e1358efa7edc1521fe
Michel Maia <programador.michel.maia@gmail.com>
2023-05-04 14:13:37 -0300

    commit

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Passa informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Propriedade é decorada com @input',
+      autoria: 'componente filho',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, non aliquam laborum voluptatum eius assumenda, architecto incidunt nam placeat et, sequi magni facilis asperiores officiis doloribus. Autem quasi iure voluptate.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, non aliquam laborum voluptatum eius assumenda, architecto incidunt nam placeat et, sequi magni facilis asperiores officiis doloribus. Autem quasi iure voluptate.',
+      autoria: 'teste',
+      modelo: 'modelo1'
+    }
+   ];
+
   constructor() { }
 
   ngOnInit(): void {
